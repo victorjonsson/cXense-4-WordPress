d685986d455f1dc1064e263852a366e0bca2139e
Tom Brännström <tom.brannstrom@gmail.com>
2014-02-17 14:19:35 +0100

    bugg-fix

--- ajax-search/ajax-search-shortcode.php
+++ ajax-search/ajax-search-shortcode.php
@@ -4,7 +4,7 @@
 function cxense_output_ajax_search() {
     wp_enqueue_script('cxense-ajax-search', CXENSE_PLUGIN_URL.'/ajax-search/search.js', array('jquery'), CXENSE_PLUGIN_VERSION);
     $data = array('post_url' => plugin_dir_url(__FILE__).'ajax/cxense-api-search.php');
-    wp_localize_script('cxense-search', 'cxense_data', $data);
+    wp_localize_script('cxense-ajax-search', 'cxense_data', $data);
     ?>
     <div id="search-wrapper">
         <input type="text" placeholder="Sök..." name="search_term" id="search-term">
