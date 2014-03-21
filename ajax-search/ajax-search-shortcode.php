f04acccfc01a7d55b6b4cb33d853d0b2d05b014b
Tom Brännström <tom.brannstrom@gmail.com>
2014-03-21 09:22:00 +0100

    markup

--- ajax-search/ajax-search-shortcode.php
+++ ajax-search/ajax-search-shortcode.php
@@ -2,16 +2,18 @@
 
 
 function cxense_output_ajax_search() {
-    wp_enqueue_script('cxense-ajax-search', CXENSE_PLUGIN_URL.'/ajax-search/search.js', array('jquery'), CXENSE_PLUGIN_VERSION);
-    $data = array('post_url' => plugin_dir_url(__FILE__).'ajax/cxense-api-search.php');
+    wp_enqueue_script('cxense-ajax-search', CXENSE_PLUGIN_URL.'ajax-search/search.js', array('jquery'), CXENSE_PLUGIN_VERSION);
+    _log(CXENSE_PLUGIN_URL.'ajax-search/search.js');
+    $data = array('post_url' => plugin_dir_url(__FILE__).'api.php');
     wp_localize_script('cxense-ajax-search', 'cxense_data', $data);
     ?>
-    <div id="search-wrapper">
+    <div class="search-wrapper">
         <input type="text" placeholder="Sök..." name="search_term" id="search-term">
-        <input type="button" class="button-primary" onclick="cxenseSearch();" value="Sök">
-        <input type="button" class="button-secondary" onclick="toggleAdvanced();" value="Inställningar">
+        <input type="button" class="btn btn--dark" onclick="cxenseSearch();" value="Sök">
+        <input type="button" class="btn btn--dark search-settings" onclick="toggleAdvanced();" value="Inställningar">
     </div>
-    <div  id="advanced-search-form" style="display: none" type="text">
+
+    <div id="advanced-search-form">
         <div id="search-options">
             <span>Sök på:</span>
             <input type="checkbox" checked="checked" name="author"> Författare |
