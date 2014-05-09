2e51c013ceb4be234a37f8520dc32292a0331ae8
Victor Jonsson <kontakt@victorjonsson.se>
2014-05-09 13:00:52 +0200

    Tools for pushing large quantites of URL:s to cXense (wip)

---
+++ push/README.md
@@ -0,0 +1,50 @@
+
+## cXense Push Utilities (wip)
+
+This directory contains utility scripts that can be used to register large quantities of URL:s
+at cXense (https://wiki.cxense.com/pages/viewpage.action?pageId=18843212).
+
+**export.php** — Used to export URL:s from WordPress
+
+**push.js** - Nodejs script used to register a bunch of URL:s at cXense
+
+
+## Usage
+
+#### Extract URL:s from WordPress
+
+Navigate to this directory in your console and run any of the commands below. The extracted URL:s will be
+written to a file name *urls.json* in the same directory. Each URL will be separated with a new line.
+
+This script will extract 500 posts per second (to reduce allocated resources) until all URL:s is extracted. Running
+this script several times will not create duplications of the extracted URL:s.
+
+`$ php export.php` Extracts all URL:s to *urls.json* separated with a new line
+
+`$ php export.php livsstil` Extracts all URL:s of posts related to the category "livsstil"
+
+
+#### Ping cXense crawler (push.js)
+
+This node-script will parse a file named "urls.json" that should be located in the same directory and contain URL:s separated with a new line.
+You can either create this file manually yourself or generate it with extract script described above.
+
+**urls.json**
+
+```
+http://mywebsite.com/23932/an-url-of-some-sort
+http://mywebsite.com/1990/another-url-of-some-sort
+...
+```
+
+**Running the script**
+
+```
+$ node push.js
+
+- Parsing urls.json
+- 13291 URL:s loaded into memory
+* Pushed http://mywebsite.com/23932/an-url-of-some-sort successfully
+* Pushed http://mywebsite.com/1990/another-url-of-some-sort successfully
+...
+```
---
+++ push/export.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * File used to export URL:s to WordPress posts and saves them to file urls.json
+ *
+ * Usage:
+ *  $ php ./export.php sport    # extract all urls from category "sport"
+ *  $ php ./export 231          # extract all urls from category with id 231
+ *  $ php ./export              # extract all urls
+ *
+ * This script will extract 500 posts per second (to reduce allocated resources)
+ * until all URLs of the posts in the declared category is extracted
+ *
+ * Running this script several times will not create duplications of the extracted
+ * URL:s. So you can run this script several times and the same urls will
+ *
+ */
+
+// Short hand function for outputting text
+function _p($str, $err=false) { fwrite($err ? STDOUT:STDERR, $str . PHP_EOL); }
+
+// Setup server vars expected to exist by wordpress
+$_SERVER['DOCUMENT_ROOT'] = getcwd();
+$_SERVER['SERVER_PROTOCOL'] = '';
+$_SERVER['HTTP_HOST'] = '127.0.0.1';
+$_SERVER['REQUEST_METHOD'] = 'GET';
+
+// Bootstrap WP
+require_once( current(explode('wp-content', __FILE__)) .'/wp-load.php' );
+
+// Do we have a category
+$cat_name = current( array_splice($argv, -1) );
+$cat_id = null;
+if( basename($cat_name) != 'export.php' ) {
+    if( is_numeric($cat_name) ) {
+        $cat_id = $cat_name;
+    } else {
+        $cat = get_term_by('name', $cat_name, 'category');
+        if( !$cat ) {
+            _p('It does not exist any category with given name '.escapeshellarg($cat_name), true);
+            die;
+        } else {
+            $cat_id = $cat->term_id;
+        }
+    }
+}
+
+$export_file = __DIR__.'/urls.json';
+$extracted_urls = file_exists($export_file) ? explode('\n', file_get_contents($export_file)) : array();
+array_flip($extracted_urls); // has entries as keys to prevent duplication when running script several times
+$offset = 0;
+$limit = 100;
+
+_p('----> About to extract URLs '.( $cat_id ? 'from category '.$cat_id:''));
+
+while( $posts = get_posts(array('posts_per_page' => $limit, 'offset'=> $offset, 'category' => $cat_id)) ) {
+    _p('* About to extract URLs from '.$offset.' to '.($offset+$limit));
+    foreach($posts as $p) {
+        if( $url = get_permalink($p) )
+            $extracted_urls[$url] = 1;
+    }
+    $offset += $limit;
+    sleep(1);
+}
+
+// Write to file
+file_put_contents($export_file, implode(PHP_EOL, array_keys($extracted_urls)));
+
+global $wpdb;
+if( isset($wpdb->num_queries) ) {
+    _p('(Database queries: '.$wpdb->num_queries.')');
+}
+_p('----> Finished extracting URLs to '.$export_file);
